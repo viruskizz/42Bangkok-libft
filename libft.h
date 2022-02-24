3650ed57bd7e9fb558d09f6dc4dbf3c8cee028a4
viruskizz <kizzaraiva@gmail.com>
2022-02-24 22:40:41 +0700

    fin part2

--- libft.h
+++ libft.h
@@ -14,6 +14,7 @@
 # define LIBFT_H
 
 # include <stdio.h>
+# include <unistd.h>
 # include <stddef.h>
 # include <stdlib.h>
 
@@ -49,5 +50,10 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
