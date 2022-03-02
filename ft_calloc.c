a702f406a877b95edd3d9f8a29fab3bbe281bbc0
Thitiwut Somsa <tsomsa@z3t7c4.42bangkok.com>
2022-03-02 23:35:51 +0700

    fix calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: tsomsa <tsomsa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 22:18:11 by tsomsa            #+#    #+#             */
-/*   Updated: 2022/02/22 22:18:12 by tsomsa           ###   ########.fr       */
+/*   Updated: 2022/03/02 23:11:07 by tsomsa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	*ft_calloc(size_t n, size_t size)
 {
 	void	*buf;
 
-	if (n == 0 || size == 0)
-		return (NULL);
 	buf = malloc(n * size);
 	if (buf)
 		ft_bzero(buf, n * size);
